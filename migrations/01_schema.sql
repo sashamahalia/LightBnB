d8ccb0d7ccab9dc0895282fab8a58e72cf5a59e2
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-31 23:50:25 +0000

    Fixes bug in migrations file and imports seed data

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -5,29 +5,29 @@ DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(225) NOT NULL,
-  email VARCHAR(225) NOT NULL,
-  password VARCHAR(225) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
-  title VARCHAR(225) NOT NULL,
+  title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_photo_url VARCHAR(225) NOT NULL,
-  cover_photo_url VARCHAR(225) NOT NULL,
-  cost_per_night INTEGER NOT NULL DEFAULT 0,
-  parking_spaces INTEGER NOT NULL DEFAULT 0,
-  number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
-  number_of_bedroorms INTEGER NOT NULL DEFAULT 0,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
-  country VARCHAR(225) NOT NULL,
-  street VARCHAR(225) NOT NULL,
-  city VARCHAR(225) NOT NULL,
-  province VARCHAR(225) NOT NULL,
-  post_code VARCHAR(225) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
 
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
